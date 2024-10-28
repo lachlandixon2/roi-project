e8ff861bd959d12859deb1249a1fb86599407651
lachlandixon2 <lachlan.dixon2@studytafensw.edu.au>
2024-10-28 13:00:44 +1100

    added navigation

---
+++ screens/ErrorScreen.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+
+export default function ErrorScreen(props) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Error Screen</Text>
+      <Button
+        title='Go Back'
+        onPress={() => {
+          props.navigation.goBack();
+        }}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+});
